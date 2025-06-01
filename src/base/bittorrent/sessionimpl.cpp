86bec18bdc6590f543e1e346d08e440b3a7fe054
orionmeetartemis <orionmeetartemis@gmail.com>
2025-06-01 16:08:12 +0800

    修改名称

--- src/base/bittorrent/sessionimpl.cpp
+++ src/base/bittorrent/sessionimpl.cpp
@@ -122,7 +122,7 @@ const int MAX_PROCESSING_RESUMEDATA_COUNT = 50;
 namespace
 {
     const char PEER_ID[] = "qB";
-    const auto USER_AGENT = QStringLiteral("qBittorrent Enhanced/" QBT_VERSION_2);
+    const auto USER_AGENT = QStringLiteral("qBittorrent/" QBT_VERSION_2);
     const QString DEFAULT_DHT_BOOTSTRAP_NODES = u"dht.libtorrent.org:25401, dht.transmissionbt.com:6881, router.bittorrent.com:6881"_s;
 
     void torrentQueuePositionUp(const lt::torrent_handle &handle)
